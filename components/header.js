aca737b526787210c65804445888b5f808be2a2a
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-01-10 09:51:16 -0600

    initial commit

---
+++ components/header.js
@@ -0,0 +1,7 @@
+export default function Header(){
+  return (<div>
+    <h1 className='text-blue-700 font-bold text-5xl'>
+      nextjs w/ tailwindcss
+    </h1>
+  </div>)
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <p>Ipsum minim culpa nostrud Lorem pariatur aute excepteur laboris ea veniam esse non velit. Cupidatat cupidatat ipsum ad et aliqua excepteur fugiat. Velit eiusmod nostrud sint officia. Non velit esse ipsum tempor cupidatat reprehenderit proident deserunt in labore velit.</p>
+  )
+}
