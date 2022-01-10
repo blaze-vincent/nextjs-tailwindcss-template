c1bb71bb5ef926d1897a1313be5cdccd1a1a4ebb
Blaze Vincent <blaze.vincent@hotmail.com>
2022-01-10 17:01:53 -0600

    Remove extraneous import; Update tag text contents for clarity

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
-    <p>Ipsum minim culpa nostrud Lorem pariatur aute excepteur laboris ea veniam esse non velit. Cupidatat cupidatat ipsum ad et aliqua excepteur fugiat. Velit eiusmod nostrud sint officia. Non velit esse ipsum tempor cupidatat reprehenderit proident deserunt in labore velit.</p>
+    <p>page content populating main tag in layout</p>
   )
 }
