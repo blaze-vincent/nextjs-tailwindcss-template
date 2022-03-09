430f171676ed397156d8ddb8a24a6df016c883fd
Blaze Vincent <blaze.vincent@hotmail.com>
2022-03-09 10:36:28 -0600

    Use semantic tags for header+footer

--- components/header.js
+++ components/header.js
@@ -1,7 +1,7 @@
 export default function Header(){
-  return (<div>
+  return (<header>
     <h1 className="text-blue-700 text-2xl font-bold">
       header tag content
     </h1>
-  </div>)
+  </header>)
 }
