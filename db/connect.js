0bdfa753d246d40fa75244967f7ca18221136b62
blaze@drakehs <blaze.vincent@drakehs.com>
2022-09-27 10:49:54 -0500

    Add database connection caching

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
-export default async function dbConnect(){
-  await mongoose.connect(process.env.MONGO_URI);
+let cached = global.mongoose
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+export default async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI).then(mongoose => {
+      return mongoose
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
 }
